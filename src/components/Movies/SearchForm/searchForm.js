579f4ec7ff56e5bb6cdf7a6e583437f045c4224a
Vlada Salinikova <iastoboii@gmail.com>
2022-10-07 15:03:52 +0300

    fix: исправить ошибки после ревью #2

--- src/components/Movies/SearchForm/searchForm.js
+++ src/components/Movies/SearchForm/searchForm.js
@@ -5,10 +5,10 @@ import Button from '../../../images/button-invisible.png';
 function SearchForm() {
   return (
     <section className="seach">
-      <div className="seach__container">
+      <form className="seach__container">
         <input className="seach__input" type="text" placeholder="Фильм" required/>
         <button className="seach__button" title="Найти"><img className="seach__button-img" src={Button} alt="Найти"/></button>
-      </div>
+      </form>
       <div className="seach__switch">
         <input type="checkbox" className="seach__checkbox" />
         <h3 className="seach__checkbox-name">Короткометражки</h3>
